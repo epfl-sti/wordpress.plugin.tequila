742e73dd249aa1388fa6eca67ebc6b3d1f215b2e
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-11-09 20:44:56 +0100

    Refactor a lot
    * Use a namespace
    * Split out a Settings class that is responsible for (wait for it) settings
    * No more top-level callbacks, make them methods instead
    * Remove dead code, commented out code

--- EPFL-Tequila.php
+++ EPFL-Tequila.php
@@ -7,6 +7,7 @@
  * Author URI:  mailto:dominique.quatravaux@epfl.ch
  */
 
+namespace EPFL\Tequila;
 
 if (! defined('ABSPATH')) {
     die('Access denied.');
@@ -14,10 +15,10 @@ if (! defined('ABSPATH')) {
 
 require_once(dirname(__FILE__) . "/tequila_client.php");
 
-class TequilaLogin
+class Controller
 {
     public static $instance = false;
-    public $prefix = 'tequila_';
+    var $settings = null;
 
     public static function getInstance()
     {
@@ -27,172 +28,16 @@ class TequilaLogin
         return self::$instance;
     }
 
+    public function __construct () {
+        $this->settings = new Settings();
+    }
+
     public function hook()
     {
         add_action('init', array($this, 'maybe_back_from_tequila'));
-        add_action('admin_menu', array($this, 'action_admin_menu'));
-        add_action('admin_init', array($this, 'action_admin_init'));
         add_action('wp_authenticate', array( $this, 'start_authentication' ));
+        $this->settings->hook();
     }
-
-    public function get_option($name, $default = false, $use_cache = true)
-    {
-        if ($this->is_network_version()) {
-            return get_site_option($name, $default, $use_cache);
-        } else {
-            return get_option($name, $default);
-        }
-    }
-
-    /**
-     * Returns whether this plugin is currently network activated
-     */
-    public $_is_network_version = null;
-    public function is_network_version()
-    {
-        if ($this->_is_network_version === null) {
-            if (! function_exists('is_plugin_active_for_network')) {
-                require_once(ABSPATH . '/wp-admin/includes/plugin.php');
-            }
-
-            $this->_is_network_version = (bool) is_plugin_active_for_network(plugin_basename(__FILE__));
-        }
-
-        return $this->_is_network_version;
-    }
-
-    public function action_admin_init()
-    {
-        // Use the settings API rather than writing our own <form>s and
-        // validators therefor.
-        // More at https://wordpress.stackexchange.com/a/100137
-        $option_name   = 'plugin:epfl-tequila';
-
-        // Fetch existing options.
-        $option_values = get_option($option_name);
-
-        $default_values = array(
-            'groups'    => 'stiitweb',
-            'faculty'   => 'STI',
-            'long'      => ''
-        );
-
-        // Parse option values into predefined keys, throw the rest away.
-        $data = shortcode_atts($default_values, $option_values);
-
-        register_setting(
-            'plugin:epfl-tequila-optiongroup',          // group, used for settings_fields()
-            $option_name,                               // option name, used as key in database
-            'validate_settings_cb'                      // validation callback
-        );
-
-        /* No argument has any relation to the prvious register_setting(). */
-        add_settings_section(
-            'section_1',                                // ID
-            'A propos',                                 // Title
-            'epfl_tequila_render_section_about',        // print output
-            'epfl_tequila'                              // menu slug, see action_admin_menu()
-        );
-
-        add_settings_section(
-            'section_2',                                // ID
-            'Aide',                                     // Title
-            'epfl_tequila_render_section_help',         // print output
-            'epfl_tequila'                              // menu slug, see action_admin_menu()
-        );
-
-        add_settings_section(
-            'section_3',                                // ID
-            'Paramètres',                               // Title
-            'epfl_tequila_render_section_parameters',   // print output
-            'epfl_tequila'                              // menu slug, see action_admin_menu()
-        );
-
-        add_settings_field(
-            'section_3_field_1',                        // ID
-            'Faculté',                                  // Title
-            'epfl_tequila_render_dropdown',             // print output
-            'epfl_tequila',                             // menu slug, see action_admin_menu()
-            'section_3',                                // parent section
-            array(
-                'label_for'   => 'faculty', // makes the field name clickable,
-                'name'        => 'faculty', // value for 'name' attribute
-                'value'       => esc_attr($data['faculty']),
-                'options'     => array(
-                        'ENAC'      => 'Architecture, Civil and Environmental Engineering — ENAC',
-                        'SB'        => 'Basic Sciences — SB',
-                        'STI'       => 'Engineering — STI',
-                        'IC'        => 'Computer and Communication Sciences — IC',
-                        'SV'        => 'Life Sciences — SV',
-                        'CDM'       => 'Management of Technology — CDM',
-                        'CDH'       => 'College of Humanities — CDH'
-                    ),
-                'option_name' => $option_name,
-                'help' => 'Permet de sélectionner les accès par défaut (droit wordpress.faculté).'
-            )
-        );
-
-        add_settings_field(
-            'section_3_field_2',                        // ID
-            'Groupes administrateur',                   // Title
-            'epfl_tequila_render_input',                // print output
-            'epfl_tequila',                             // menu slug, see action_admin_menu()
-            'section_3',                                // parent section
-            array(
-                'label_for'   => 'groups', // makes the field name clickable,
-                'name'        => 'groups', // value for 'name' attribute
-                'value'       => esc_attr($data['groups']),
-                'option_name' => $option_name,
-                'help' => 'Groupe permettant l’accès administrateur.'
-            )
-        );
-    }
-
-    public function validate_settings_cb()
-    {
-        if (false) {
-            add_settings_error(
-                'plugin:epfl-tequila-optiongroup',
-                'number-too-low',
-                'Number must be between 1 and 1000.'
-            );
-        }
-    }
-
-    public function eg_setting_info()
-    {
-        echo '<input name="eg_setting_name" id="eg_setting_name" type="checkbox" value="1" class="code" ' . checked(1, get_option('eg_setting_name'), false) . ' /> Explanation text';
-    }
-
-    // Spit out every knob previously registered with
-    // https://wordpress.stackexchange.com/questions/100023/settings-api-with-arrays-example
-    public function action_admin_menu()
-    {
-        add_options_page(
-            __('Réglages de Tequila', 'epfl-tequila'),  // $page_title,
-            __('Tequila (auth)', 'epfl-tequila'),       // $menu_title,
-            'manage_options',                           // $capability,
-            'epfl_tequila',                             // $menu_slug
-            array($this, 'render_settings')             // Callback
-        );
-    }
-
-    public function render_settings()
-    {
-        ?>
-        <div class="wrap">
-            <h2><?php print $GLOBALS['title']; ?></h2>
-            <form action="options.php" method="POST">
-                <?php
-                    settings_fields('plugin:epfl-tequila-optiongroup');
-                    do_settings_sections('epfl_tequila');
-                    submit_button();
-                ?>
-            </form>
-        </div>
-        <?php
-    }
-
 
     public function start_authentication()
     {
@@ -243,79 +88,250 @@ class TequilaLogin
     }
 }
 
-function epfl_tequila_render_section_about() //t5_sae_render_section_1()
+Controller::getInstance()->hook();
+
+class Settings
 {
-    echo __('<p><a href="https://github.com/epfl-sti/wordpress.plugin.tequila">EPFL-tequila</a>
+    function hook()
+    { 
+        add_action('admin_init', array($this, 'action_admin_init'));
+        add_action('admin_menu', array($this, 'action_admin_menu'));
+    }
+
+    public function get($name, $default = false, $use_cache = true)
+    {
+        if ( $this->is_network_version() ) {
+            return get_site_option($name, $default, $use_cache);
+        } else {
+            return get_option($name, $default);
+        }
+    }
+
+    public function get_option($name, $default = false, $use_cache = true)
+    {
+        if ($this->is_network_version()) {
+            return get_site_option($name, $default, $use_cache);
+        } else {
+            return get_option($name, $default);
+        }
+    }
+
+    /**
+     * @returns Whether this plugin is currently network activated
+     */
+    public $_is_network_version = null;
+    public function is_network_version()
+    {
+        if ($this->_is_network_version === null) {
+            if (! function_exists('is_plugin_active_for_network')) {
+                require_once(ABSPATH . '/wp-admin/includes/plugin.php');
+            }
+
+            $this->_is_network_version = (bool) is_plugin_active_for_network(plugin_basename(__FILE__));
+        }
+
+        return $this->_is_network_version;
+    }
+
+    public function action_admin_init()
+    {
+        // Use the settings API rather than writing our own <form>s and
+        // validators therefor.
+        // More at https://wordpress.stackexchange.com/a/100137
+        $option_name   = 'plugin:epfl-tequila';
+
+        // Fetch existing options.
+        $option_values = $this->get( $option_name );
+
+        $default_values = array(
+            'groups'    => 'stiitweb',
+            'faculty'   => 'STI',
+            'long'      => ''
+        );
+
+        // Parse option values into predefined keys, throw the rest away.
+        $data = shortcode_atts($default_values, $option_values);
+
+        register_setting(
+            'plugin:epfl-tequila-optiongroup',          // group, used for settings_fields()
+            $option_name,                               // option name, used as key in database
+            array($this, 'validate_settings')           // validation callback
+        );
+
+        /* No argument has any relation to the prvious register_setting(). */
+        add_settings_section(
+            'section_1',                                // ID
+            'A propos',                                 // Title
+            array($this, 'render_section_about'),       // print output
+            'epfl_tequila'                              // menu slug, see action_admin_menu()
+        );
+
+        add_settings_section(
+            'section_2',                                // ID
+            'Aide',                                     // Title
+            array($this, 'render_section_help'),        // print output
+            'epfl_tequila'                              // menu slug, see action_admin_menu()
+        );
+
+        add_settings_section(
+            'section_3',                                // ID
+            'Paramètres',                               // Title
+            array($this, 'render_section_parameters'),  // print output
+            'epfl_tequila'                              // menu slug, see action_admin_menu()
+        );
+
+        add_settings_field(
+            'section_3_field_1',                        // ID
+            'Faculté',                                  // Title
+            array($this, 'render_dropdown'),            // print output
+            'epfl_tequila',                             // menu slug, see action_admin_menu()
+            'section_3',                                // parent section
+            array(
+                'label_for'   => 'faculty', // makes the field name clickable,
+                'name'        => 'faculty', // value for 'name' attribute
+                'value'       => esc_attr($data['faculty']),
+                'options'     => array(
+                    'ENAC'      => 'Architecture, Civil and Environmental Engineering — ENAC',
+                    'SB'        => 'Basic Sciences — SB',
+                    'STI'       => 'Engineering — STI',
+                    'IC'        => 'Computer and Communication Sciences — IC',
+                    'SV'        => 'Life Sciences — SV',
+                    'CDM'       => 'Management of Technology — CDM',
+                    'CDH'       => 'College of Humanities — CDH'
+                ),
+                'option_name' => $option_name,
+                'help' => 'Permet de sélectionner les accès par défaut (droit wordpress.faculté).'
+            )
+        );
+
+        add_settings_field(
+            'section_3_field_2',                        // ID
+            'Groupes administrateur',                   // Title
+            array($this, 'render_input'),               // print output
+            'epfl_tequila',                             // menu slug, see action_admin_menu()
+            'section_3',                                // parent section
+            array(
+                'label_for'   => 'groups',
+                'name'        => 'groups',
+                'value'       => esc_attr($data['groups']),
+                'option_name' => $option_name,
+                'help' => 'Groupe permettant l’accès administrateur.'
+            )
+        );
+    }
+
+    /**
+     * Spit out every knob previously registered with action_admin_init()
+     *
+     * @see https://wordpress.stackexchange.com/questions/100023/settings-api-with-arrays-example
+     */
+    public function action_admin_menu()
+    {
+        add_options_page(
+            __('Réglages de Tequila', 'epfl-tequila'),  // $page_title,
+            __('Tequila (auth)', 'epfl-tequila'),       // $menu_title,
+            'manage_options',                           // $capability,
+            'epfl_tequila',                             // $menu_slug
+            array($this, 'render')                      // Callback
+        );
+    }
+
+    public function render()
+    {
+        ?>
+        <div class="wrap">
+        <h2><?php print $GLOBALS['title']; ?></h2>
+        <form action="options.php" method="POST">
+<?php
+        settings_fields('plugin:epfl-tequila-optiongroup');
+        do_settings_sections('epfl_tequila');
+        submit_button();
+        ?>
+        </form>
+        </div>
+<?php
+    }
+
+    public function validate_settings()
+    {
+        if (false) {
+            add_settings_error(
+                'plugin:epfl-tequila-optiongroup',
+                'number-too-low',
+                'Number must be between 1 and 1000.'
+            );
+        }
+    }
+
+    function render_section_about()
+    {
+        echo __('<p><a href="https://github.com/epfl-sti/wordpress.plugin.tequila">EPFL-tequila</a>
     permet l’utilisation de <a href="https://tequila.epfl.ch/">Tequila</a>
     (Tequila est un système fédéré de gestion d’identité. Il fournit les moyens
     d’authentifier des personnes dans un réseau d’organisations) avec
     WordPress.</p>', 'epfl-tequila');
-}
+    }
 
-function epfl_tequila_render_section_help() //t5_sae_render_section_2()
-{
-    echo __('<p>En cas de problème avec EPFL-tequila veuillez créer une
+    function render_section_help()
+    {
+        echo __('<p>En cas de problème avec EPFL-tequila veuillez créer une
     <a href="https://github.com/epfl-sti/wordpress.plugin.tequila/issues/new"
     target="_blank">issue</a> sur le dépôt
     <a href="https://github.com/epfl-sti/wordpress.plugin.tequila/issues">
     GitHub</a>.</p>', 'epfl-tequila');
-}
-
-function epfl_tequila_render_input($args)
-{
-    /* Creates this markup:
-    /* <input name="plugin:t5_sae_option_name[number]"
-     */
-    printf(
-        '<input name="%1$s[%2$s]" id="%3$s" value="%4$s" class="regular-text">',
-        $args['option_name'],
-        $args['name'],
-        $args['label_for'],
-        $args['value']
-    );
-    // t5_sae_debug_var( func_get_args(), __FUNCTION__ );
-    if ($args['help']) {
-        echo '<br />&nbsp;<i>' . $args['help'] . '</i>';
     }
-}
 
-function epfl_tequila_render_dropdown($args)
-{
-    printf(
-        '<select name="%1$s[%2$s]" id="%3$s">',
-        $args['option_name'],
-        $args['name'],
-        $args['label_for']
-    );
-
-    foreach ($args['options'] as $val => $title) {
+    function render_input($args)
+    {
+        /* Creates this markup:
+           /* <input name="plugin:option_name[number]"
+        */
         printf(
-            '<option value="%1$s" %2$s>%3$s</option>',
-            $val,
-            selected($val, $args['value'], false),
-            $title
+            '<input name="%1$s[%2$s]" id="%3$s" value="%4$s" class="regular-text">',
+            $args['option_name'],
+            $args['name'],
+            $args['label_for'],
+            $args['value']
         );
-    }
-    print '</select>';
-    if ($args['help']) {
-        echo '<br />&nbsp;<i>' . $args['help'] . '</i>';
+        if ($args['help']) {
+            echo '<br />&nbsp;<i>' . $args['help'] . '</i>';
+        }
     }
 
-    // t5_sae_debug_var( func_get_args(), __FUNCTION__ );
+    function render_dropdown($args)
+    {
+        printf(
+            '<select name="%1$s[%2$s]" id="%3$s">',
+            $args['option_name'],
+            $args['name'],
+            $args['label_for']
+        );
+
+        foreach ($args['options'] as $val => $title) {
+            printf(
+                '<option value="%1$s" %2$s>%3$s</option>',
+                $val,
+                selected($val, $args['value'], false),
+                $title
+            );
+        }
+        print '</select>';
+        if ($args['help']) {
+            echo '<br />&nbsp;<i>' . $args['help'] . '</i>';
+        }
+    }
+
+    function render_textarea($args)
+    {
+        printf(
+            '<textarea name="%1$s[%2$s]" id="%3$s" rows="10" cols="30" class="code">%4$s</textarea>',
+            $args['option_name'],
+            $args['name'],
+            $args['label_for'],
+            $args['value']
+        );
+        if ($args['help']) {
+            echo '<br />&nbsp;<i>' . $args['help'] . '</i>';
+        }
+    }
 }
-
-function epfl_tequila_render_textarea($args)
-{
-    printf(
-        '<textarea name="%1$s[%2$s]" id="%3$s" rows="10" cols="30" class="code">%4$s</textarea>',
-        $args['option_name'],
-        $args['name'],
-        $args['label_for'],
-        $args['value']
-    );
-    if ($args['help']) {
-        echo '<br />&nbsp;<i>' . $args['help'] . '</i>';
-    }
-}
-
-TequilaLogin::getInstance()->hook();
