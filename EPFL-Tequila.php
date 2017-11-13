4c514636b0d7feae80256daec58c6b21bcf25a36
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-11-13 20:04:33 +0100

    Bump version

--- EPFL-Tequila.php
+++ EPFL-Tequila.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: EPFL Tequila
  * Description: Authenticate to WordPress with Tequila
- * Version:     0.2
+ * Version:     0.3
  * Author:      Dominique Quatravaux
  * Author URI:  mailto:dominique.quatravaux@epfl.ch
  */
