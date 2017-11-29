c0e1d69e2e548e3461cd771423f207d42cb25a82
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-11-29 19:36:27 +0100

    Different style for comments that should remain so

--- site.php
+++ site.php
@@ -10,10 +10,10 @@ if (! defined('ABSPATH')) {
 
 // Controller::getInstance()->is_debug_enabled = true;
 
-// Uncomment to do what it says on the tin.
+/* Uncomment to do what it says on the tin. */
 // Controller::getInstance()->use_test_tequila = true;
 
-// Uncomment this to lock down the admin page for this plugin completely.
+/* Uncomment this to lock down the admin page for this plugin completely. */
 // Configuration is still feasible using the CLI.
 
 // Controller::getInstance()->settings->is_configurable = false;
