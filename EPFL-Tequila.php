3e967caf5185b20ef267dc3716aa01e956f1e2f8
LuluTchab <l.chaboudez@gmail.com>
2018-11-15 14:06:40 +0100

    Update version number and add (vpsi) in it

--- EPFL-Tequila.php
+++ EPFL-Tequila.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: EPFL Tequila
  * Description: Authenticate to WordPress with Tequila
- * Version:     0.12
+ * Version:     0.13 (vpsi)
  * Author:      Dominique Quatravaux
  * Author URI:  mailto:dominique.quatravaux@epfl.ch
  */
